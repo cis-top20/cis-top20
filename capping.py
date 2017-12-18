9bef6d8cf57f6312241f9fdc646ab185039a987b
ggarcia <ggarcia@crowdprocess.com>
2017-12-18 21:22:42 +0000

    Added a missing space

--- capping.py
+++ capping.py
@@ -8,7 +8,7 @@ def capping(df, cap, weight_column='weight', tol=1e-3, inplace=False):
         return df
 
     df.loc[~capped, weight_column] *= 1 + \
-        (df.loc[capped, weight_column]-cap).sum()/ \
+        (df.loc[capped, weight_column]-cap).sum() / \
         df.loc[~capped, weight_column].sum()
     df.loc[capped, weight_column] = cap
 
