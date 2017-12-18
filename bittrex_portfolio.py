8c6378aaf5e30d1b7944ee7271edce91f68ebd29
ggarcia <ggarcia@crowdprocess.com>
2017-12-18 21:20:36 +0000

    Added get_capping() function

--- bittrex_portfolio.py
+++ bittrex_portfolio.py
@@ -1,6 +1,9 @@
 import os
 import pandas as pd
+from coinmarketcap import Market
 from bittrex.bittrex import Bittrex, API_V2_0, API_V1_1
+
+from capping import capping
 
 
 def get_balance_list(balances):
@@ -25,20 +28,23 @@ def get_balance_list(balances):
 def get_balance_df(balance_list, btc_usd_price=None):
     balance_df = pd.DataFrame(
         balance_list,
-        columns=['currency', 'available', 'balance', 'BTC_value'])
+        columns=['symbol', 'available', 'balance', 'BTC_value'])
 
     if btc_usd_price:
         balance_df['USD_value'] = balance_df['BTC_value'] * btc_usd_price
     total_btc = balance_df['BTC_value'].sum()
-    balance_df['weight'] = balance_df['BTC_value'] / total_btc
+    balance_df['current_weight'] = balance_df['BTC_value'] / total_btc
 
-    return balance_df.sort_values(by='weight')
+    balance_df.set_index('symbol', inplace=True)
+
+    return balance_df.sort_values(by='current_weight', ascending=False)
 
 
 class BittrexPortfolio(object):
     """docstring for BittrexPortfolio."""
 
-    def __init__(self):
+    def __init__(self, n_coins=20, cap=0.1):
+        self.coinmarketcap = Market()
         self.BITTREX_API_KEY = os.getenv('BITTREX_API_KEY')
         self.BITTREX_API_SECRET = os.getenv('BITTREX_API_SECRET')
         self.v1_bittrex = Bittrex('', '', api_version=API_V1_1)
@@ -46,6 +52,9 @@ class BittrexPortfolio(object):
             self.BITTREX_API_KEY,
             self.BITTREX_API_SECRET,
             api_version=API_V2_0)
+        self.blacklist = pd.Series(['BCC', 'USDT'])
+        self.n_coins = n_coins
+        self.cap = cap
 
     def get_balances(self):
         """Returns a DataFrame with all available balances."""
@@ -68,3 +77,26 @@ class BittrexPortfolio(object):
         balances = self.v2_bittrex.get_balances()
         balance_list = get_balance_list(balances)
         return get_balance_df(balance_list)['BTC_value'].sum()
+
+    def get_capping(self):
+        """Returns portfolio capping objective."""
+        market_data = self.coinmarketcap.ticker(limit=50)
+        bittrex_markets = pd.DataFrame(self.v1_bittrex.get_markets()['result'])
+        bittrex_coins = bittrex_markets.loc[
+            bittrex_markets['BaseCurrency'] == 'BTC']['MarketCurrency']
+        bittrex_coins = bittrex_coins.append(pd.Series('BTC'))
+
+        df = pd.DataFrame(market_data)
+        df = df.loc[
+            df['symbol'].isin(bittrex_coins) &
+            ~df['symbol'].isin(self.blacklist), :]
+
+        df = df.head(self.n_coins)
+
+        # compute market weights
+        df['market_cap_usd'] = df['market_cap_usd'].astype(float)
+        df['weight'] = df['market_cap_usd']/df['market_cap_usd'].sum()
+
+        # compute capped weights
+        capped = capping(df, self.cap, weight_column='weight')
+        return capped[['symbol', 'weight']].set_index('symbol')
